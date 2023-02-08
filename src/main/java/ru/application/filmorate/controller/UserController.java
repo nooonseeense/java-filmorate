ba1148fa03a7c4c340c10e951c46c6bdd3efc673
nooonseeense <egor-bolt12@yandex.ru>
2023-02-08 20:28:17 +0300

    feat: new branch init(3)

--- src/main/java/ru/application/filmorate/controller/UserController.java
+++ src/main/java/ru/application/filmorate/controller/UserController.java
@@ -1,72 +1,33 @@
 package ru.application.filmorate.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import ru.application.filmorate.exception.UserValidationException;
 import ru.application.filmorate.model.User;
 import org.springframework.web.bind.annotation.*;
+import ru.application.filmorate.service.UserService;
 
 import javax.validation.Valid;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RequestMapping("/users")
 @RestController
-@Slf4j
 public class UserController {
-    private final Map<Integer, User> users = new HashMap<>();
-    private int userId = 1;
+    private final UserService userService;
 
-    private int generatorId() {
-        return userId++;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping
-    public User createUser(@Valid @RequestBody User userFromRequest) {
-        log.info("Получен запрос 'POST /users'");
-        if (users.containsValue(userFromRequest)) {
-            String exceptionMessage = "Пользователь уже зарегестрирован.";
-            log.warn("Ошибка при добавлении пользователя. Текст исключения: {}", exceptionMessage);
-            throw new UserValidationException(exceptionMessage);
-        }
-        User user = validationUser(userFromRequest);
-        user.setId(generatorId());
-        users.put(user.getId(), user);
-        log.info("Пользователь создан.");
-        return user;
+    public User createUser(@Valid @RequestBody User user) {
+        return userService.createUser(user);
     }
 
     @PutMapping
-    public User updateUser(@Valid @RequestBody User userFromRequest) {
-        log.info("Получен запрос 'PUT /users'");
-        if (!users.containsKey(userFromRequest.getId())) {
-            String exceptionMessage = "Такого пользователя нет в списке.";
-            log.warn("Ошибка при обновлении пользователя. Текст исключения: {}", exceptionMessage);
-            throw new UserValidationException(exceptionMessage);
-        }
-        User user = validationUser(userFromRequest);
-        users.remove(userFromRequest.getId());
-        users.put(user.getId(), user);
-        log.info("Пользователь обновлен.");
-        return user;
-    }
-
-    private User validationUser(User user) {
-        if (user.getLogin().matches(".*\\s+.*")) {
-            String exceptionMessage = "Логин не может содержать пробелы.";
-            log.warn("Ошибка при валидации пользователя. Текст исключения: {}", exceptionMessage);
-            throw new UserValidationException(exceptionMessage);
-        }
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-        return user;
+    public User updateUser(@Valid @RequestBody User user) {
+        return userService.updateUser(user);
     }
 
     @GetMapping
     public List<User> getUsers() {
-        log.info("Получен запрос 'GET /users'");
-        log.debug("Текущее количество пользователей: {}", users.size());
-        return List.copyOf(users.values());
+        return userService.getUsers();
     }
 }
--- src/main/java/ru/application/filmorate/service/FilmService.java
+++ src/main/java/ru/application/filmorate/service/FilmService.java
@@ -1,7 +1,68 @@
 package ru.application.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.application.filmorate.exception.FilmValidationException;
+import ru.application.filmorate.model.Film;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @Service
 public class FilmService {
+    private final static LocalDate THE_OLDEST_RELEASE_DATE = LocalDate.of(1895, Month.DECEMBER, 28);
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int filmId = 1;
+
+    public Film addFilm(Film filmFromRequest) {
+        log.info("Получен запрос 'POST /films'");
+        if (films.containsValue(filmFromRequest)) {
+            String exceptionMessage = "Фильм с таким названием " + filmFromRequest.getName() + " уже добавлен.";
+            log.warn("Ошибка при добавлении фильма. Текст исключения: {}", exceptionMessage);
+            throw new FilmValidationException(exceptionMessage);
+        }
+        Film film = validateFilm(filmFromRequest);
+        film.setId(generatorId());
+        films.put(film.getId(), film);
+        log.info("Фильм был добавлен.");
+        return film;
+    }
+
+    public Film updateFilm(Film filmFromRequest) {
+        log.info("Получен запрос 'PUT /films'");
+        if (!films.containsKey(filmFromRequest.getId())) {
+            String exceptionMessage = "Такого фильма нет в списке.";
+            log.warn("Ошибка при обновлении фильма. Текст исключения: {}", exceptionMessage);
+            throw new FilmValidationException(exceptionMessage);
+        }
+        Film film = validateFilm(filmFromRequest);
+        films.remove(filmFromRequest.getId());
+        films.put(film.getId(), film);
+        log.info("Фильм был обновлен.");
+        return film;
+    }
+
+    public List<Film> getFilms() {
+        log.info("Получен запрос 'GET /films'");
+        log.debug("Текущее количество фильмов: {}", films.size());
+        return List.copyOf(films.values());
+    }
+
+    private int generatorId() {
+        return filmId++;
+    }
+
+    private Film validateFilm(Film film) {
+        String exceptionMessage;
+        if (film.getReleaseDate().isBefore(THE_OLDEST_RELEASE_DATE)) {
+            exceptionMessage = "Дата релиза не может быть раньше " + THE_OLDEST_RELEASE_DATE;
+            log.warn("Ошибка при валидации фильма. Текст исключения: {}", exceptionMessage);
+            throw new FilmValidationException(exceptionMessage);
+        }
+        return film;
+    }
 }
