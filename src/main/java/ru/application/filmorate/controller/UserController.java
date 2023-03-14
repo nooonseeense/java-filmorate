046d52b871bedae41a57b6655e90a10c755e8bb8
pLumbum82 <jobbsy@yandex.ru>
2023-03-14 22:50:50 +0500

    first commit, test group project

--- src/main/java/ru/application/filmorate/controller/UserController.java
+++ src/main/java/ru/application/filmorate/controller/UserController.java
@@ -59,4 +59,9 @@ public class UserController {
                               @PositiveOrZero @PathVariable Integer friendId) {
         userService.removeFriends(id, friendId);
     }
+
+    @DeleteMapping("{userId}")
+    public void deleteUserById(@PositiveOrZero @PathVariable Integer userId) {
+        //userService.getById(userId);
+    }
 }
