6d37eded4309b3864c8afb27555db0b2315299f2
nooonseeense <egor-bolt12@yandex.ru>
2023-03-14 20:08:11 +0300

    init develop

--- src/main/java/ru/application/filmorate/FilmorateApplication.java
+++ src/main/java/ru/application/filmorate/FilmorateApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilmorateApplication {
     public static void main(String[] args) {
+        // init
         SpringApplication.run(FilmorateApplication.class, args);
     }
 }
