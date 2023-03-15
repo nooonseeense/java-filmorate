cfce50488a662e1ac8ce98b147c209addbf54316
nooonseeense <egor-bolt12@yandex.ru>
2023-03-15 20:07:10 +0300

    init add-director

--- src/main/java/ru/application/filmorate/FilmorateApplication.java
+++ src/main/java/ru/application/filmorate/FilmorateApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilmorateApplication {
     public static void main(String[] args) {
-        // init
+        // init_add_director
         SpringApplication.run(FilmorateApplication.class, args);
     }
 }
