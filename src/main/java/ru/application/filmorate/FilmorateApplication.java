8d03d0c5e855f49b0742e1761b9108f7e781255a
nooonseeense <egor-bolt12@yandex.ru>
2023-03-19 18:12:46 +0300

    feat: Получить изменения с девелоп и запушить.

--- src/main/java/ru/application/filmorate/FilmorateApplication.java
+++ src/main/java/ru/application/filmorate/FilmorateApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilmorateApplication {
     public static void main(String[] args) {
-        // init_add_director
+        // init_add_director_2
         SpringApplication.run(FilmorateApplication.class, args);
     }
 }
